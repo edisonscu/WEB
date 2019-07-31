08959c6c73f51bf6ebbce76d56c1f4a55e285ecd
ChiuanTest-01 <tusna08124@gmail.com>
2019-07-31 16:27:36 +0800

    <edison did>

--- server/app.py
+++ server/app.py
@@ -4,9 +4,9 @@ import json
 from time import sleep
 
 import pymongo
-client = pymongo.MongoClient(host='localhost', port=27017)
-db = client.test
-collection = db.flask
+client = pymongo.MongoClient(host='127.0.0.1', port=27017)
+db = client.flask
+collection = db.flask_test
 
 app = Flask(__name__)
 CORS(app)
@@ -67,9 +67,9 @@ def put_user():
     else:
          return json.dumps({"status":"get user forbidden","user":user_id}),403
 
-@app.route('/user',methods=['login_POST'])#POST
-def login_user():
-    user_id = request.args['user_id']
+@app.route('/user/<id>/login',methods=['POST'])#POST
+def login_user(id):
+    user_id = id
     print(user_id)
     result = collection.find_one({'user_id': user_id})
     if (result != None):
