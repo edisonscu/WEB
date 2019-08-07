bf56ac7aad86fc86263d0f84261548561d2e8fc2
chuang861012 <33791604+chuang861012@users.noreply.github.com>
2019-08-07 16:49:17 +0800

    init login

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,8 +5,5 @@ import App from './App';
 const root = document.getElementById('root');
 if (root) {
     const token = root.dataset.token;
-    ReactDOM.render(<App token={token} />, root);
+    ReactDOM.render(<App token={token!} />, root);
 }
-
-
-
